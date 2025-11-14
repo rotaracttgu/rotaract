43bc79e6799abb4ec61d1388be45a2c5e2a4d2f0
Carlos Daniel Interiano Irías <cinteriano25@gmail.com>
2025-11-14 00:31:54 -0600

    Revert "ij"
    This reverts commit e650aec957cb5334cc08ac619d4be98684a87066.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,27 +1,29 @@
 <?php
 
 use Illuminate\Foundation\Application;
-use Illuminate\Contracts\Http\Kernel;
-use Illuminate\Contracts\Console\Kernel as ConsoleKernel;
-use Illuminate\Contracts\Debug\ExceptionHandler;
+use Illuminate\Foundation\Configuration\Exceptions;
+use Illuminate\Foundation\Configuration\Middleware;
 
-$app = new Application(
-    $_ENV['APP_BASE_PATH'] ?? dirname(__DIR__)
-);
+return Application::configure(basePath: dirname(__DIR__))
+    ->withRouting(
+        web: __DIR__.'/../routes/web.php',
+        commands: __DIR__.'/../routes/console.php',
+        health: '/up',
+    )
+    ->withMiddleware(function (Middleware $middleware): void {
+        // Registrar middleware de Spatie
+        $middleware->alias([
+            'role' => \Spatie\Permission\Middleware\RoleMiddleware::class,
+            'permission' => \Spatie\Permission\Middleware\PermissionMiddleware::class,
+            'role_or_permission' => \Spatie\Permission\Middleware\RoleOrPermissionMiddleware::class,
+        ]);
 
-$app->singleton(
-    Illuminate\Contracts\Http\Kernel::class,
-    App\Http\Kernel::class
-);
-
-$app->singleton(
-    Illuminate\Contracts\Console\Kernel::class,
-    App\Console\Kernel::class
-);
-
-$app->singleton(
-    Illuminate\Contracts\Debug\ExceptionHandler::class,
-    App\Exceptions\Handler::class
-);
-
-return $app;
+        // Registrar middleware personalizado
+        $middleware->alias([
+            'ensure.role' => \App\Http\Middleware\EnsureUserHasRole::class,
+            'check.first.login' => \App\Http\Middleware\CheckFirstLogin::class, // ⭐ NUEVO
+        ]);
+    })
+    ->withExceptions(function (Exceptions $exceptions): void {
+        //
+    })->create();
