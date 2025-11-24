f138f29ac87d31343055ec0c5fa9e44b5749ee49
Carlos Daniel Interiano Irías <cinteriano25@gmail.com>
2025-11-24 00:07:38 -0600

    UI: Add Font Awesome CDN to tesorero layout for icon visibility

--- resources/views/modulos/tesorero/layout.blade.php
+++ resources/views/modulos/tesorero/layout.blade.php
@@ -6,6 +6,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ config('app.name', 'Laravel') }} - Tesorero</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     @stack('styles')
 </head>
